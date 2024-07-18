1f4c629949bad548bb0b286c829972c4fdc646b7
Stack-Sage <adarsh.pathania.04@gmail.com>
2024-07-18 18:34:09 +0530

    Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -66,7 +66,7 @@ const Body = () => {
               onClick={() => handleDownload(image.urls.full)}
               className="absolute bottom-1 right-1 p-2 shadow-md rounded-full invert bg-white"
             >
-              <img src="/src/assets/eye.svg" alt="eye" className="w-4 h-4 hover:scale-125 animate-pulse transform ease-in-out " />
+              <img src="eye.svg" alt="eye" className="w-4 h-4 hover:scale-125 animate-pulse transform ease-in-out " />
             </button>
           </Tilt>
         ))}
