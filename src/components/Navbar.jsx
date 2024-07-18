d4ee4d3be1a8b9f6abc52f1d6c7cbe45ad35d918
Stack-Sage <adarsh.pathania.04@gmail.com>
2024-07-18 20:04:21 +0530

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
 
 
+
 const Navbar = () => {
   return (
     <div className='h-[40px] sm:h-[60px] border-2 rounded-md border-black w-screen bg-gradient-to-b from-purple-900 via-purple-500 to-purple-300 flex absolute top-0 gap-5 p-4  text-center justify-center'>
       <div className="flex gap-2 items-center  " >
+{/*         
+         <img className= "  h-[50px] w-[45px] " src="keet.svg " alt="" /> */}
+
+
+
+<div class="flex h-[40px] items-center justify-center ">
+  <div class="w-max">
+    <h1
+      class="animate-typing overflow-hidden font-serif whitespace-nowrap border-r-4 border-r-white pr-5 lg:text-3xl md:text-2xl sm:text-xl  text-transparent bg-gradient-to-tr from-blue-900 via-blue-800 to-black font-bold bg-clip-text shadow-pink-600" >
+      Keet - Copyright Free Images 
+    </h1>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+         
         
-         <img className= "  h-[50px] w-[45px] " src="keet.svg " alt="" />
-         <div className="flex font-bold text-black text-lg ">
-          Keet - Copyright Free Images  
-         </div>
         
       </div>
       
